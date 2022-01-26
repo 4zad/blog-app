eb7f1d523e410a309b63da4ba3fb8790b973e639
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-01-25 21:43:48 -0500

    Data files initialized and necessary headers added
    Header added to server.js. posts.json and categories.json files initialized.

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic 
+* Policy. No part of this assignment has been copied manually or electronically 
+* from any other source (including web sites) or distributed to other students.
+*
+* Name: Muhammad Ahmed Student 
+* ID: 146908207 
+* Date: 01-14-2022
+*
+* Online (Heroku) URL: 
+*
+********************************************************************************/
+
 const express = require("express");
 const app = express();
 const path = require("path");
