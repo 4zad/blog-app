4940348bbf71b34aae1083c0a75eb71b9e92688b
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-01-30 18:07:21 -0500

    Unit tested methods in the 'blog-service' module and fixed realted bugs
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- blog-service.js
+++ blog-service.js
@@ -4,13 +4,13 @@ const path = require("path");
 var posts = [];
 var categories = [];
 
-module.exports.initialize(() => {
+module.exports.initialize = (() => {
     return new Promise((resolve, reject) => {
         try {
             fs.readFile(path.join(__dirname, "/data/posts.json"), "utf8", (err, data) => {
                 if (err) throw `ERROR: ${err}`;
                 posts = JSON.parse(data);
-                
+
                 fs.readFile(path.join(__dirname, "/data/categories.json"), "utf8", (err, data) => {
                     if (err) throw `ERROR: ${err}`;
                     categories = JSON.parse(data);
@@ -18,19 +18,19 @@ module.exports.initialize(() => {
 
                 resolve();
             });
-        } catch (err) { 
+        } catch (err) {
             reject(err);
-        }        
+        }
     });
 });
 
-module.exports.getAllPosts(() => {
+module.exports.getAllPosts = (() => {
     return new Promise((resolve, reject) => {
         posts.length != 0 ? resolve(posts) : reject("ERROR: No data returned. There may not be any data to display.");
     });
 });
 
-module.exports.getPublishedPosts(() => {
+module.exports.getPublishedPosts = (() => {
     return new Promise((resolve, reject) => {
         let publishedPosts = [];
 
@@ -42,10 +42,35 @@ module.exports.getPublishedPosts(() => {
     });
 });
 
-module.exports.getCategories(() => {
+module.exports.getCategories = (() => {
     return new Promise((resolve, reject) => {
         categories.length != 0 ? resolve(categories) : reject("ERROR: No data returned. There may not be any data to display.");
     });
 });
 
 
+
+
+// Unit testing on 'initialize' method
+/*
+const read = (() => {
+    return new Promise((resolve,reject) => {
+        fs.readFile(path.join(__dirname, "/data/categories.json"), "utf8", (err, data) => {
+            categories = JSON.parse(data);
+            resolve(categories);
+        });
+    });
+});
+
+myArray = [
+    { fName: "Fred", lName: "Flintstone" },
+    { fName: "Wilma", lName: "Flintstone" },
+    { fName: "Barney", lName: "Rubble" },
+];
+
+read().then((array) => {
+    console.log(array);
+    console.log(typeof (array));
+    console.log(typeof (myArray));
+});
+*/
