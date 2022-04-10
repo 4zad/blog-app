ac4a1597c143178601bf077d63be99207343b8cc
Muhammad Ahmed <mahmed.4zad@gmail.com>
2022-04-10 13:32:00 -0400

    Finished creation of 'auth-service' module
    The 'auth-service' custom module has been completed but still requires testing. This module currently holds methods to register user accounts and to verify user account login credentials, which is required for login functionality.
    
    Signed-off-by: Muhammad Ahmed <mahmed.4zad@gmail.com>

--- auth-service.js
+++ auth-service.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // schema to define a user account 
 let userSchema = new Schema({
-    "userName": {
+    "username": {
         "type": String,
         "unique": true
     },
@@ -19,8 +19,96 @@ let userSchema = new Schema({
 // declare variable to hold model of the 'userSchema' schema
 let User; // to be defined on creation of new connection in 'initialize' method
 
+// defining the MongoDB database credentials
+const dbName = "dbBlogApp";
+const dbUsername = "dbUser";
+const dbPassword = "dbPassword";
+
+// Defining module methods...
+module.exports.initialize = () => {
+    return new Promise((resolve, reject) => {
+        // create a connection to the MongoDB database, defined in 'db' with the specified connection string sent as an argument
+        let db = mongoose.createConnection(`mongodb+srv://${dbUsername}:${dbPassword}@senecaweb.owgrb.mongodb.net/${dbName}?retryWrites=true&w=majority`);
+
+        // verify the 'db' connection
+        db.on("error", (err) => {
+            reject(err); // reject the promise with the provided error
+        });
+        db.once("open", () => {
+            User = db.model("users", userSchema); // define the user account model using the 'userSchema' schema and associating it with the collection called 'users' on the database
+            resolve(`SUCCESS: The operation succeeded at successfully connecting to the '${dbName}' database.`);
+        });
+    });
+}
+
+module.exports.registerUser = (userData) => {
+    return new Promise((resolve, reject) => {
+        if (userData.password != userData.password2) {
+            reject(`ERROR: Passwords do not match.`);
+        } else {
+            let newUser = new User({
+                username: userData.username,
+                password: userData.password,
+                email: userData.email
+            });
+
+            newUser.save((err, newUserData) => {
+                if (err) {
+                    if (err == 11000) {
+                        reject(`ERROR: The username '${newUserData.username}' is already taken.`);
+                    } else {
+                        reject(`ERROR: The user could not be created. The following error occurred: \n${err}`);
+                    }
+                } else {
+                    resolve(`SUCCESS: The following user was successfully created: \n${newUserData}`);
+                }
+            });
+        }
+    });
+}
+
+module.exports.checkUser = (userData) => {
+    return new Promise((resolve, reject) => {
+        User.find({
+            username: userData.username
+        }).exec().then((users) => {
+            if (users.length == 0) {
+                reject(`ERROR: Unable to find user, '${userData.username}'.`);
+            } else {
+                bcrypt.compare(users[0].password, userData.password, (err, match) => {
+                    if (match === false) {
+                        reject(`ERROR: Incorrect password for user, '${userData.username}'.`);
+                    } else if (res === true) {
+                        if (users[0].loginHistory == null) {
+                            users[0].loginHistory = [];
+                        }
+
+                        users[0].loginHistory.push({
+                            dateTime: (new Date()).toString(), userAgent: userData.userAgent
+                        });
+                        User.updateOne({
+                            username: users[0].username
+                        }, {
+                            $set: {
+                                loginHistory: users[0].loginHistory
+                            }
+                        }).exec()
+                            .then(() => {
+                                resolve(users[0]);
+                            })
+                            .catch((err) => {
+                                reject(`ERROR: There was a problem verifying the user: \n${err}`);
+                            });
+                    }
+                });
+            }
+        }).catch(() => {
+            reject(`ERROR: Unable to find user, '${userData.username}'.`);
+        });
+    });
+}
 
 
-User = mongoose.model("users", userSchema);
-// mongodb+srv://dbUser:dbPassword@senecaweb.owgrb.mongodb.net/dbBlogApp?retryWrites=true&w=majority
+
+
 
